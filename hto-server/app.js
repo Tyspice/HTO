addf2fd7261f04e0a5dc3e8a4b46d1ee5685300c
Tyspice <tyner27@gmail.com>
2021-09-04 13:16:05 -0700

    add npm scripts

--- hto-server/app.js
+++ hto-server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const api = require(api);
+const api = require('./api');
 
 app.get('/', (req, res) => {
   res.send('Water\'s not ready yet')
