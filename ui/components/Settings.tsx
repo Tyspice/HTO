a3cd7234cfd5371ea89f8ee54ec293dee8db2a4a
Tyspice <tyner27@gmail.com>
2022-02-19 19:32:46 -0800

    rough settings page done

--- ui/components/Settings.tsx
+++ ui/components/Settings.tsx
@@ -1,10 +1,57 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { ReactElement } from "react";
+import { StyleSheet, View } from "react-native";
 import { Layout, Text } from "@ui-kitten/components";
+import { Divider, List, ListItem, Input } from "@ui-kitten/components";
+
+interface Setting {
+  title: string;
+  input?: () => ReactElement;
+}
+
+const ServerInput = () => {
+  return <Input placeholder="<ip>" />
+};
+
+const AuthInput= () => {
+  return <Input placeholder="<token>" /> 
+};
+
+const settings: Setting[] = [
+  {
+    title: "Server IP",
+    input: ServerInput,
+  },
+  {
+    title: "Auth Token",
+    input: AuthInput,
+  },
+];
+
+const renderItem = ({ item }: { item: Setting }) => (
+  <ListItem title={item.title} accessoryRight={item.input} />
+);
+
+const SettingsList = () => (
+  <View style={styles.settingsListContainer}>
+    <List
+      style={styles.settingsList}
+      ItemSeparatorComponent={Divider}
+      data={settings}
+      renderItem={renderItem}
+    />
+  </View>
+);
+
+const Title = () => (
+  <View style={ styles.titleContainer }>
+    <Text style={ styles.title }>SETTINGS</Text>
+  </View>
+);
 
 export const Settings = () => (
   <Layout style={styles.container}>
-    <Text>This Is The Settings Screen</Text>
+    <Title />
+    <SettingsList />
   </Layout>
 );
 
@@ -13,7 +60,22 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 20,
     paddingBottom: 40,
-    justifyContent: "space-around",
+    justifyContent: "flex-start",
     alignItems: "center",
+  },
+  settingsListContainer: {
+    borderColor: "black",
+    borderWidth: 0.5,
+    width: "90%",
+  },
+  settingsList: {
+    maxHeight: 200,
+  },
+  title: {
+    fontSize: 20,
+  },
+  titleContainer: {
+    marginTop: 25,
+    padding: 50
   },
 });
